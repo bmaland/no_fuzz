086c6ea0242c1d7f992e0cf0182d557ce896f44b
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-04-09 02:42:16 +0200

    Bugfix for foreign key ref, added a test

--- test/no_fuzz_test.rb
+++ test/no_fuzz_test.rb
@@ -4,15 +4,21 @@ class NoFuzzTest < ActiveSupport::TestCase
 
   load_schema
 
-  class Package < ActiveRecord::Base
-  end
-
   class PackageTrigram < ActiveRecord::Base
   end
 
-  def test_schema_has_loaded_correctly
-    assert_equal [], Package.all
-    assert_equal [], PackageTrigram.all
+  class Package < ActiveRecord::Base
+    include NoFuzz
+
+    fuzzy :name
+  end
+
+  def test_populating_and_deleting_trigram_index
+    Package.create!(:name => "abcdef")
+    Package.populate_trigram_index
+    assert_equal 5, PackageTrigram.count
+    Package.clear_trigram_index
+    assert_equal 0, PackageTrigram.count
   end
 
 end
